d667e61bd6bbb002699068dfa7703b7d546164c2
itspamelie <pamelamartinez2003@hotmail.com>
2026-03-21 12:50:08 -0600

    Seeders Finalizados
    Correccion de errores en los insert.

--- api/database/seeders/UserRoutineSessionsSeeder.php
+++ api/database/seeders/UserRoutineSessionsSeeder.php
@@ -13,25 +13,25 @@ class UserRoutineSessionsSeeder extends Seeder
      */
     public function run(): void
     {
-         DB::table('user_routine_sessions')->insert([
-            [
-                'id' => 1,
-                'user_id' => 3,
-                'routine_id' => 1,
-                'started_at' => now()->subDays(2),
-                'finished_at' => now()->subDays(2)->addHour(),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 2,
-                'user_id' => 3,
-                'user_routine_id' => 1,
-                'started_at' => now()->subDay(),
-                'finished_at' => now()->subDay()->addHour(),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ]);
+DB::table('user_routine_sessions')->insert([
+    [
+        'id' => 1,
+        'user_id' => 3,
+        'routine_id' => 1,
+        'started_at' => now()->subDays(2),
+        'finished_at' => now()->subDays(2)->addHour(),
+        'created_at' => now(),
+        'updated_at' => now(),
+    ],
+    [
+        'id' => 2,
+        'user_id' => 3,
+        'routine_id' => 1,
+        'started_at' => now()->subDay(),
+        'finished_at' => now()->subDay()->addHour(),
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]
+]);
     }
 }
--- api/database/seeders/UserScheduleSeeder.php
+++ api/database/seeders/UserScheduleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserScheduleSeeder extends Seeder
 {
@@ -12,14 +13,32 @@ class UserScheduleSeeder extends Seeder
      */
     public function run(): void
     {
-          DB::table('user_schedule')->insert([
-            // USER 3 (PRO)
-            ['user_id'=>3,'routine_id'=>1,'day_of_week'=>'monday'],
-            ['user_id'=>3,'routine_id'=>2,'day_of_week'=>'wednesday'],
-            ['user_id'=>3,'user_routine_id'=>1,'day_of_week'=>'friday'],
-
-            // USER 4 (FREE)
-            ['user_id'=>4,'routine_id'=>1,'day_of_week'=>'tuesday'],
-        ]);
+DB::table('user_schedule')->insert([
+    // USER 3 (PRO)
+    [
+        'user_id'=>3,
+        'routine_id'=>1,
+        'user_routine_id'=>null,
+        'day_of_week'=>'monday'
+    ],
+    [
+        'user_id'=>3,
+        'routine_id'=>2,
+        'user_routine_id'=>null,
+        'day_of_week'=>'wednesday'
+    ],
+    [
+        'user_id'=>3,
+        'routine_id'=>null,
+        'user_routine_id'=>1,
+        'day_of_week'=>'friday'
+    ],
+    [
+        'user_id'=>3,
+        'routine_id'=>1,
+        'user_routine_id'=>null,
+        'day_of_week'=>'tuesday'
+    ],
+]);
     }
 }
